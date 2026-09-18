1f8c8e5b35e9cb094fa88f506579695ac28a38d1
guoxu1111 <guoxu34@h-partners.com>
2026-09-18 17:39:31 +0800

    update: 更新文件 selection_service_test.cpp 修复6.1LTS分支ut问题 Co-authored-by: Agent
    Signed-off-by: guoxu1111 <guoxu34@h-partners.com>

--- test/unittest/selection_service_test.cpp
+++ test/unittest/selection_service_test.cpp
@@ -54,12 +54,12 @@ private:
     sptr<IRemoteObject> GetSelectionSystemAbility();
 };
 
-void DbSelectionConfigRepositoryTest::SetUpTestCase()
+void SelectionServiceTest::SetUpTestCase()
 {
     std::system("param set sys.selection.switch on");
     std::system("mkdir -p /data/service/el1/public/selection_service");
     std::system("chmod 0770 /data/service/el1/public/selection_service");
-    std::cout << "DbSelectionConfigRepositoryTest SetUpTestCase" << std::endl;
+    std::cout << "SelectionServiceTest SetUpTestCase" << std::endl;
 }
 
 void SelectionServiceTest::TearDownTestCase()
