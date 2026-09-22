6b886c3aa61f0a9e768271b0301ca117492eae99
guoxu1111 <guoxu34@h-partners.com>
2026-09-22 11:43:33 +0800

    update: 更新文件 db_selection_config_repository_test.cpp
    Signed-off-by: guoxu1111 <guoxu34@h-partners.com>

--- test/unittest/db_selection_config_repository_test.cpp
+++ test/unittest/db_selection_config_repository_test.cpp
@@ -51,7 +51,8 @@ public:
 void DbSelectionConfigRepositoryTest::SetUpTestCase()
 {
     std::system("param set sys.selection.switch on");
-    std::system("mkdir -p /data/service/el1/public/selection_service");
+    std::system("mkdir /data/service/el1/public/selection_service 0770 sysselection sysselection");
+    std::system("chown sysselection sysselection /data/service/el1/public/selection_service");
     std::system("chmod 0770 /data/service/el1/public/selection_service");
     std::cout << "DbSelectionConfigRepositoryTest SetUpTestCase" << std::endl;
 }
